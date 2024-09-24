fb7f7283cd1dabfc97ed9719277fee9d4b9d365a
tomyinn <tomas.brazdil05@gmail.com>
2024-09-24 07:43:26 +0200

    hello_world

---
+++ PainVape/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/authenticator', function () {
+    return view('authenticator');
+});
+
---
+++ git_config.sh
@@ -0,0 +1,4 @@
+#!/bin/shcd example-app
+git config --config user.name "Tomyinn"
+git config --config user.name "tomas.brazdil05@gmail.com"
+echo 
