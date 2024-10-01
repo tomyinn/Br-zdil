162954ca60228e6e60d08cd11832fee421983c93
tomyinn <tomas.brazdil05@gmail.com>
2024-10-01 08:04:54 +0200

    git config --global -unset-all user.name

--- git_config.sh
+++ git_config.sh
@@ -1,4 +1,9 @@
 #!/bin/shcd example-app
-git config --config user.name "Tomyinn"
+git config --list
+git config --global -unset-all user.name
+git config --global -unset-all user.mail
+git config --config user.name "tomyinn"
 git config --config user.name "tomas.brazdil05@gmail.com"
-echo 
+echo "Přihlášení proběhlo v pořádku"
+ls
+
