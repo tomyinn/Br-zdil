7a8864c5c224e83e8d940e81ec32bffa00cfbc36
tomyinn <tomas.brazdil05@gmail.com>
2024-10-01 07:55:19 +0200

    feat: :fire: hlava proti kolenu
    T

---
+++ PainVape/app/Http/Controllers/ProdatController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreprodatRequest;
+use App\Http\Requests\UpdateprodatRequest;
+use App\Models\prodat;
+
+class ProdatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreprodatRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(prodat $prodat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(prodat $prodat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateprodatRequest $request, prodat $prodat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(prodat $prodat)
+    {
+        //
+    }
+}
